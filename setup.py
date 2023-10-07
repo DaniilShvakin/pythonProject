17eb5cf37786f1cfcc0f81111774ffa5bff7a283
Home <dsshvakin@mail.ru>
2023-10-07 12:20:16 +0300

    1

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
-from setuptools import setup
 
-setup(
-    name='pythonProject',
-    version='',
-    packages=[''],
-    url='',
-    license='',
-    author='Home',
-    author_email='',
-    description=''
 )
