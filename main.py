731210b4092f5d02ed680e02e096495f7c28d065
Home <dsshvakin@mail.ru>
2023-10-15 17:41:30 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-c = input()
-d= input()
-e=float(c)+float(d)
-print(e)
+
+a=int(input())
+while a > 0:
+    import time
+    time.sleep(1)
+    print(a)
+    a = a-1
+print("Бум")
+from tkinter import *
+def change_color():
+    current_color = box.cget("background")
+    next_color = "black" if current_color == "red" else "red"
+    box.config(background=next_color)
+    root.after(100, change_color)
+root = Tk()
+box = Text(root, background="black")
+box.pack()
+change_color()
+root.mainloop()
